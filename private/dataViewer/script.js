a93d7768732a03a8c8690515061f5a40951667ee
Andreas Jonsterhaug <andreas.vj@icloud.com>
2023-04-29 23:59:13 +0200

    sorted data visualization

--- private/dataViewer/script.js
+++ private/dataViewer/script.js
@@ -3,9 +3,14 @@ import { ImageData, LabelData } from "./data.js"
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 
+const imageLabelElement = document.getElementById("imageLabelElement")
 const imageIndexInput = document.getElementById("imageIndexInput")
 const imageIndexElement = document.getElementById("imageIndexElement")
 const downloadLabelsBtn = document.getElementById("downloadLabelsBtn")
+const displayImagesBtn = document.getElementById("displayImagesBtn")
+const labelCheckboxes = document.getElementById("labelCheckboxes")
+const imagesContainer = document.getElementById("imagesContainer")
+
 
 const WIDTH = 28
 const HEIGHT = 28
@@ -14,50 +19,39 @@ const NUM_IMAGES = 81*124
 
 imageIndexInput.max = NUM_IMAGES - 1
 
-const arr = new Uint8Array(NUM_IMAGES, -1)
-for (let i = 0; i < NUM_IMAGES; i++) {
-    arr[i] = -1
-}
-
-
-function download(content, fileName, contentType) {
-    var a = document.createElement("a")
-    var file = new Blob([content], {type: contentType})
-    a.href = URL.createObjectURL(file)
-    a.download = fileName
-    a.click()
-}
-
-// download(arr, 'labels.bin', "");
-
-
 canvas.width = WIDTH
 canvas.height = HEIGHT
+
 
 const images = new ImageData(WIDTH, HEIGHT)
 images.load("images.npy").then(() => {
     ctx.putImageData(images.getCanvasImageData(imageIndexInput.value), 0, 0)
 })
 
+imageIndexInput.addEventListener("input", () => {
+    imageIndexElement.innerText = imageIndexInput.value
+    if (!images.loaded) {
+        alert("Images are not done loading")
+        return
+    }
+    if (!labels.loaded) {
+        alert("Labels are not done loading")
+        return
+    }
+
+    ctx.putImageData(images.getCanvasImageData(imageIndexInput.value), 0, 0)
+    imageLabelElement.innerText = labels.data[imageIndexInput.value]
+})
+
+
 const labels = new LabelData()
-labels.load("./labels.bin")
+labels.load("./labels.bin").then(() => {
+    imageLabelElement.innerText = labels.data[imageIndexInput.value]
+})
 
 downloadLabelsBtn.onclick = () => {
     labels.download()
 }
-
-
-
-
-imageIndexInput.addEventListener("input", () => {
-    if (images.loaded) {
-        ctx.putImageData(images.getCanvasImageData(imageIndexInput.value), 0, 0)
-    }
-    else {
-        alert("Images are not done loading")
-    }
-    imageIndexElement.innerText = imageIndexInput.value
-})
 
 
 document.addEventListener("keydown", (event) => {
@@ -65,11 +59,39 @@ document.addEventListener("keydown", (event) => {
         if (labels.loaded) {
             labels.data[imageIndexInput.value] = event.key
             imageIndexInput.value++
-            ctx.putImageData(images.getCanvasImageData(imageIndexInput.value), 0, 0)
-            console.log(labels.data)
+            imageIndexInput.dispatchEvent(new Event("input", {bubbles:true}))
         }
         else {
             alert("Labels are not done loading")
         }
     }
 })
+
+
+displayImagesBtn.onclick = () => {
+    const includedLabels = []
+        
+    for (let element of labelCheckboxes.children) {
+        if (element.type == "checkbox") {
+            includedLabels.push(element.checked)
+        }
+    }
+
+    imagesContainer.innerHTML = ""
+
+    for (let i = 0; i < NUM_IMAGES; i++) {
+        if (includedLabels[labels.data[i]]) {
+            const newCanvas = document.createElement("canvas")
+            const newCtx = newCanvas.getContext("2d")
+
+            newCanvas.id = i
+            
+            newCanvas.width = WIDTH
+            newCanvas.height = HEIGHT
+    
+            newCtx.putImageData(images.getCanvasImageData(i), 0, 0)
+            
+            imagesContainer.appendChild(newCanvas)
+        }
+    }
+}
